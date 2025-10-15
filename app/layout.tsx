c5ebe5ae67c1f1febedae81495964a4e4242c790
Amadi Jerry <amadijerry310@gmail.com>
2025-10-15 19:02:26 +0100

    minor fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,17 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { GeistSans } from "geist/font/sans"
-import { GeistMono } from "geist/font/mono"
-import { Analytics } from "@vercel/analytics/next"
-import { Suspense } from "react"
-import "./globals.css"
+import type React from "react";
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import { Analytics } from "@vercel/analytics/next";
+import { Suspense } from "react";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "ChadLoader - Download YouTube Like a Chad",
   description: "No ads. No limits. Just pure Sigma efficiency.",
+  icons: {
+    icon: "/appicon.png", 
+  },
   openGraph: {
     title: "ChadLoader - Download YouTube Like a Chad",
     description: "No ads. No limits. Just pure Sigma efficiency.",
@@ -33,18 +36,23 @@ export const metadata: Metadata = {
   },
 };
 
-
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en" className="dark">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="zmSsfd2XOrY9meRmTH2O84y8SAgWiME0IiObf4yx_jQ"
+        />
+      </head>
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
         <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
         <Analytics />
       </body>
     </html>
-  )
+  );
 }
